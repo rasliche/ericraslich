377d9bcd24900791425685e14df2b7f297ae4f02
Eric R <rasliche@gmail.com>
2019-10-01 14:30:17 -0400

    pass through brave verificaiton file

---
+++ .eleventy.js
@@ -0,0 +1,5 @@
+module.exports = function(eleventyConfig) {
+  // Copy `css/fonts/` to `_site/css/fonts`
+  // If you use a subdirectory, it’ll copy using the same directory structure.
+  eleventyConfig.addPassthroughCopy(".well-known/brave-rewards-verification.txt");
+  };
