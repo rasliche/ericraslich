ef74e2b5632ccf2b6eb7de4ee4d628e7c82d72e6
Eric R <rasliche@gmail.com>
2019-10-25 09:38:13 -0400

    comment out main function to not register service worker because I don't know how that shit works

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,15 @@
-(function() {
-    console.log('Invoke the static site template JavaScript!');
+// (function() {
+//     console.log('Invoke the static site template JavaScript!');
 
-    // ServiceWorker is a progressive technology. Ignore unsupported browswers.
-    if ('serviceWorker' in navigator) {
-        console.log('CLIENT: ServiceWorker registration in progress.');
-        navigator.serviceWorker.register('/service-worker.js').then(function() {
-            console.log('CLIENT: ServiceWorker registration complete.');
-        }, function() {
-            console.log('CLIENT: ServiceWorker registration failed.');
-        });
-    } else {
-        console.log('CLIENT: ServiceWorker is not supported.');
-    }
-})();
+//     // ServiceWorker is a progressive technology. Ignore unsupported browswers.
+//     if ('serviceWorker' in navigator) {
+//         console.log('CLIENT: ServiceWorker registration in progress.');
+//         navigator.serviceWorker.register('/service-worker.js').then(function() {
+//             console.log('CLIENT: ServiceWorker registration complete.');
+//         }, function() {
+//             console.log('CLIENT: ServiceWorker registration failed.');
+//         });
+//     } else {
+//         console.log('CLIENT: ServiceWorker is not supported.');
+//     }
+// })();
