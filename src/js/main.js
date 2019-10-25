b44fc09cc9c82f5e95e0acd6ede64b6736cfc0e9
Eric R <rasliche@gmail.com>
2019-10-25 18:39:31 -0400

    provide reference elements and attach emoji tooltips

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,28 @@
-// (function() {
-//     console.log('Invoke the static site template JavaScript!');
+(function() {
+    console.log('Invoke the static site template JavaScript!');
 
-//     // ServiceWorker is a progressive technology. Ignore unsupported browswers.
-//     if ('serviceWorker' in navigator) {
-//         console.log('CLIENT: ServiceWorker registration in progress.');
-//         navigator.serviceWorker.register('/service-worker.js').then(function() {
-//             console.log('CLIENT: ServiceWorker registration complete.');
-//         }, function() {
-//             console.log('CLIENT: ServiceWorker registration failed.');
-//         });
-//     } else {
-//         console.log('CLIENT: ServiceWorker is not supported.');
-//     }
-// })();
+    // ServiceWorker is a progressive technology. Ignore unsupported browswers.
+    // if ('serviceWorker' in navigator) {
+    //     console.log('CLIENT: ServiceWorker registration in progress.');
+    //     navigator.serviceWorker.register('/service-worker.js').then(function() {
+    //         console.log('CLIENT: ServiceWorker registration complete.');
+    //     }, function() {
+    //         console.log('CLIENT: ServiceWorker registration failed.');
+    //     });
+    // } else {
+    //     console.log('CLIENT: ServiceWorker is not supported.');
+    // }
+
+    // Emoji Tooltips
+    const tt = new Tooltip(document.querySelector('#fishing'), {
+        delay: 500,
+        trigger: 'hover focus',
+        title: '🐟'
+    })
+    const tt2 = new Tooltip(document.querySelector('#webdev'), {
+        delay: 500,
+        trigger: 'hover focus',
+        title: '💻'
+    })
+
+})();
