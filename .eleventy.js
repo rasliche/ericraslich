f8fa03508f4aabcd6fefea4de61e73c3ae2d6cbf
Eric Raslich <ericraslich@MacBook-Pro.local>
2020-05-12 14:40:04 -0400

    Add changelog shortcode
    Shamelessly copied from https://timothymiller.dev/posts/2020/adding-a-changelog-to-my-11ty-blog/#conclusion

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const gitlog = require('gitlog').default
 const { DateTime } = require("luxon")
 
 module.exports = function(eleventyConfig) {
@@ -20,6 +21,35 @@ module.exports = function(eleventyConfig) {
   })
 
   // shortcodes
+  // changelog
+  eleventyConfig.addNunjucksShortcode("changelog", ({filePath}) => {
+    // First we remove "./" from filePath
+    // This leaves us with the format "posts/2020/file.md"
+    let relPath = filePath.slice(2);
+    // Limit logs to 20, only fetch commit message and date
+    const options = {
+      repo: __dirname,
+      number: 20,
+      fields: ["subject", "authorDate"],
+      file: relPath
+    };
+    // Here's where the magic happens!
+    // We pass our params into gitlog, and it handles the rest
+    let commits = gitlog(options);
+    // Now we need to loop through the commits, and create our HTML.
+    // I use a list and a <details> element here,
+    // but you could use whatever markup you want!
+    let html = "<details><summary>Changelog</summary><ul>";
+    for (let i=0; i<commits.length;i++) {
+      // Convert the git date to ISO
+      let isoDate = commits[i].authorDate.slice(0,10);
+      // Convert ISO to readable date e.g. "May 05 2020"
+      let readableDate = DateTime.fromISO(isoDate).toFormat('LLLL dd yyyy');
+      html += `<li><time datetime="${isoDate}">${readableDate}</time> ${commits[i].subject}</li>`;
+    }
+    html += "</ul></details>";
+    return html;
+  });
 
   return {
     dir: {
