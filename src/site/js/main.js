5864b5a7dd15eebe1a3cfc19d64e964401313a44
Eric Raslich <rasliche@gmail.com>
2020-08-23 19:57:57 -0400

    Removed the service worker

--- src/site/js/main.js
+++ src/site/js/main.js
@@ -2,16 +2,16 @@
     console.log('Invoke the static site template JavaScript!');
 
     // ServiceWorker is a progressive technology. Ignore unsupported browswers.
-    if ('serviceWorker' in navigator) {
-        console.log('CLIENT: ServiceWorker registration in progress.');
-        navigator.serviceWorker.register('/service-worker.js').then(function() {
-            console.log('CLIENT: ServiceWorker registration complete.');
-        }, function() {
-            console.log('CLIENT: ServiceWorker registration failed.');
-        });
-    } else {
-        console.log('CLIENT: ServiceWorker is not supported.');
-    }
+    // if ('serviceWorker' in navigator) {
+    //     console.log('CLIENT: ServiceWorker registration in progress.');
+    //     navigator.serviceWorker.register('/service-worker.js').then(function() {
+    //         console.log('CLIENT: ServiceWorker registration complete.');
+    //     }, function() {
+    //         console.log('CLIENT: ServiceWorker registration failed.');
+    //     });
+    // } else {
+    //     console.log('CLIENT: ServiceWorker is not supported.');
+    // }
 
     // Emoji Tooltips
     // const tt = new Tooltip(document.querySelector('#fishing'), {
