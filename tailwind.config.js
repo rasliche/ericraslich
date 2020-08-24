96a153dc4cf263f69ac8419e34504df2d244be20
Eric Raslich <rasliche@gmail.com>
2020-08-24 15:25:02 -0400

    feat: use tailwindcss/typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,24 @@ module.exports = {
         // simple-price-grid-component
         'bright-yellow': 'hsl(71, 73%, 54%)'
       }
-    }
+    },
+    typography: theme => ({
+      default: {
+        css: {
+          color: theme('colors.gray.700'),
+          a: {
+            color: theme('colors.blue.600'),
+            '&:hover': {
+              color: theme('colors.blue.800')
+            }
+          }
+        }
+      }
+    })
   },
   variants: {},
   plugins: [
+    require('@tailwindcss/typography'),
     // require('.src/plugins/parallax')
   ]
 }
